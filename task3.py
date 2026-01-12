00b53dc3f402ed409cbcc5fdc0c90ee9c1bb9de8
W-SDSS <wakanai027@deltalearns.ca>
2026-01-12 09:39:11 -0800

    w

--- task3.py
+++ task3.py
@@ -1,3 +1,42 @@
 import random
 
-rock scissors paper
+choices = random.choice(["rock","scissors", "paper", "lizard", "spock"])
+user = input(print("Enter your choices, rock, scissors, paper, lizard and spock: "))
+u = True
+
+while u == True:
+    if choices == "rock":
+        if user == "izard" or "scissors":
+            print("You win!!")
+            break
+        if user == "paper" or "spock":
+            print("You lose")
+            continue
+    if choices == "scissors":
+        if user == "izard" or "paper":
+            print("You win!!")
+            break
+        if user == "rock" or "spock":
+            print("You lose")
+            continue
+    if choices == "paper":
+        if user == "rock" or "spock":
+            print("You win!!")
+            break
+        if user == "scissors" or "lizard":
+            print("You lose")
+            continue
+    if choices == "lizard":
+        if user == "spock" or "paper":
+            print("You win!!")
+            break
+        if user == "rock" or "scissors":
+            print("You lose")
+            continue
+    if choices == "spock":
+        if user == "scissors" or "rock":
+            print("You win!!")
+            break
+        if user == "lizard" or "paper":
+            print("You lose")
+            continue
