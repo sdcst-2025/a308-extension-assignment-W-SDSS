593dbbf81f2a49b78a3c4168f20b8532504a7cbc
W-SDSS <wakanai027@deltalearns.ca>
2026-01-26 19:54:30 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,18 +5,12 @@ guesscount = 0
 
 while True:
     guess = int(input("Enter your guess (1-100): "))
+    guesscount += 1
     if guess == x:
         print("You are correct!!")
         print(f"You've guessed {guesscount} times")
         break
     elif guess >= x:
         print("its too high.")
-        guesscount += 1
-        continue
     else:
         print("its too low")
-        guesscount += 1
-        continue
-
-#1 no sa demo, its too high ni shiterukedo daijyoubuka
-#diagram wo kaku
